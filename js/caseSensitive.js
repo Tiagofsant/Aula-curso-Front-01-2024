6d7af98f981bff0f5a573fb8893ed72a6a17cb0e
tiagofsant <f.santos_aer@hotmail.com>
2024-11-16 01:14:53 -0400

    feat# atividade calculadora

--- js/caseSensitive.js
+++
@@ -1,7 +0,0 @@
-function mudaNome() {
-  var nomeusuario = "Joaquim";
-  var nomeUsuario = "Amanda";
-
-  document.getElementById("nome1").innerHTML = nomeusuario;
-  document.getElementById("nome2").innerHTML = nomeUsuario;
-}
